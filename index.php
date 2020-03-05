d75036e34db3cfceb9a676466221fbab85692e97
Dan Sampson <dansampson@gmail.com>
2020-03-05 11:20:55 +0530

    put it in a class!

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Hello World';
+class MessageAlert {
+	function sendMessage( $strMessage='' ){
+		if( '' !== $strMessage ) echo $strMessage;
+	}
+}
+
+$objMessage = new MessageAlert( 'Hello World! This is better' );
