622dd44abdd77d53b29f77f07bfa99e698ff6ffa
Dan Sampson <dansampson@gmail.com>
2020-03-05 11:23:16 +0530

    updating the file so it works through a class

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'Hello World';
+
+// We should really create a class!
+class MessageAlert {
+    function sendMessage( $strMessage='' ){
+        if( '' !== $strMessage ) echo $strMessage;
+    }
+}
+
+
+$objMessageAlert = new MessageAlert();
+$objMessageAlert->sendMessage( 'Hello Universe!' );
